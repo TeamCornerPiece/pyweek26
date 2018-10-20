ead9dc6049184907b7403c2a3bb311ee60ab42aa
saidwho12 <saidmetiche2000@gmail.com>
2018-10-20 02:26:39 -0400

    added something

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
+import numpy
+from pyglfw.libapi import *
 
 if __name__ == '__main__':
-    from scripts import engine
-    engine.Engine()
+    glfwInit()
+
+    major, mintor, rev = glfwGetVersion()
+    window = glfwCreateWindow(640, 480, b'Hello World!', None, None)
+
+    while not glfwWindowShouldClose(window):
+        glfwPollEvents()
+        glfwSwapBuffers(window)
+
+
+    glfwDestroyWindow(window)
+    glfwTerminate()
+
