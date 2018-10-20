f220fbd4f5585e0c27586b8a7391784d0dc099fa
TheCornerPiece <cornerpieceofficial@gmail.com>
2018-10-19 22:18:58 -0700

    added windows venv

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 
 if __name__ == '__main__':
     from scripts import engine
-    import pyfmodex
     engine.Engine()
--- scripts/engine.py
+++ scripts/engine.py
@@ -1,5 +1,15 @@
+from openal import *
+import time
 
 
 class Engine:
     def __init__(self):
-        pass
+        source = oalOpen("test.wav")
+
+        source.play()
+
+        while source.get_state() == AL_PLAYING:
+            # wait until the file is done playing
+            time.sleep(1)
+
+        oalQuit()
