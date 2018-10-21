90b9c19b0a85664923271935f28d871331881866
saidwho12 <saidmetiche2000@gmail.com>
2018-10-20 23:04:24 -0400

    added shader and pipeline functions.

--- gl.py
+++ gl.py
@@ -6,15 +6,25 @@ def createShader(string: str, type: any):
     glShaderSource(shader, string)
     glCompileShader(shader)
 
-    success = glGetShaderiv(GL_COMPILE_STATUS)
+    success = glGetShaderiv(shader=shader, pname=GL_COMPILE_STATUS)
     if success == GL_FALSE:
-        print(glGetShaderInfoLog(shader).decode('utf-8'))
+        print(glGetShaderInfoLog(obj=shader).decode('utf-8'))
         glDeleteShader(shader)
 
     return shader
 
+
 def createPipeline(shaders: list):
     program = glCreateProgram()
     for shader in shaders:
-        glLinkProgram(program, shader)
+        glAttachShader(program, shader)
 
+    glLinkProgram(program)
+    success = glGetProgramiv(program=program, pname=GL_LINK_STATUS)
+    if success == GL_FALSE:
+        print(glGetProgramInfoLog(obj=program).decode('utf-8'))
+
+        for shader in shaders: glDetachShader(program, shader)
+        glDeleteProgram(program)
+
+    return program
--- main.py
+++ main.py
@@ -1,4 +1,24 @@
-from scripts import engine
+from pyglfw.libapi import *
+from gl import *
 
 if __name__ == '__main__':
-    engine.Engine()
+    glfwInit()
+
+    major, mintor, rev = glfwGetVersion()
+    window = glfwCreateWindow(1280, 720, b'Hello World!', None, None)
+    glfwMakeContextCurrent(window)
+
+    vertexShader = createShader(open('shaders/default.vert', 'r').read(), GL_VERTEX_SHADER)
+    fragmentShader = createShader(open('shaders/default.frag', 'r').read(), GL_FRAGMENT_SHADER)
+
+    graphicsPipeline = createPipeline([vertexShader, fragmentShader])
+
+    while not glfwWindowShouldClose(window):
+
+
+        glfwPollEvents()
+        glfwSwapBuffers(window)
+
+
+    glfwDestroyWindow(window)
+    glfwTerminate()
